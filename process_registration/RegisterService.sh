3e598f15ebf617d6c3bccfa4d81a8bbedb566d84
Grabt234 <gnorrie12@gmail.com>
2024-07-24 05:23:22 +0200

    Fix: Registering services correctly

--- process_registration/RegisterService.sh
+++ process_registration/RegisterService.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # ========================================
+# Check we have passed arguments
+# ========================================
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided - No service registered"
+    exit 1
+fi
+
+# ========================================
 # Do some admin to prepare service files
 # ========================================
 
@@ -18,11 +27,13 @@ sed -i "s/{Name}/$process_name/g" "$process_name.service"
 # Create directory in /opt if it doesn't exist
 mkdir -p "/opt/$process_name"
 # Move the new service file to /opt/{process_name}
-mv "$process_name.service" "/opt/$process_name/"
+mv "$process_name.service" "/etc/systemd/system/"
 # Move binary named {process_name} to /opt/{process_name}
-mv "$process_name" "/opt/$process_name/"
+cp "$process_name" "$process_name.used"
+mv "$process_name" "/opt/$process_name/$process_name"
 # Move config file into the same location
-mv "Config.json" "/opt/$process_name/"
+cp "Config.json" "Config.json.used"
+mv "Config.json" "/opt/$process_name/Config.json"
 # Change permissions of this binary to +wrx
 chmod +wrx "/opt/$process_name/$process_name"
 
@@ -34,7 +45,5 @@ chmod +wrx "/opt/$process_name/$process_name"
 systemctl daemon-reload
 # Enable the service
 systemctl enable "$process_name.service"
-# Reload the service
-systemctl reload "$process_name.service"
 # Start the service
 systemctl start "$process_name.service"
